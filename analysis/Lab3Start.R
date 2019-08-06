8e3d0e2ec8f0a55cd7bb355d9f395f4695da73ce
Landcruiser87 <andyheroy@gmail.com>
2019-08-06 11:28:30 -0500

    Turned "data" into a transactional set.  NOt sure if i need too, but oh well.

--- analysis/Lab3Start.R
+++ analysis/Lab3Start.R
@@ -13,28 +13,38 @@ data <- AdultUCI[, -c(3,5,11:12)]
 
 # colnames(data)[colnames(data)=="capital-gain"] <- "capgain"
 # colnames(data)[colnames(data)=="capital-loss"] <- "caploss"
-
 colnames(data)[colnames(data)=="hours-per-week"] <- "hoursperweek"
 
 # 
 # data$capgain <- with(data,impute(capgain,median))
 # data$caploss <- with(data,impute(caploss,median))
 
-data$age <- cut(data$age, breaks = c(0,15,30,45,65,90), labels =c("Young", "YoungAdult", "MiddleAge", "Senior", "Retired") )
+data$age <- cut(data$age, breaks = c(15,25,45,65,100), labels =c("Young", "Middleaged", "Senior", "Retired"))
 data$hoursperweek <- cut(data$hoursperweek, breaks = c(0,20,40,60,80), labels =c("part-time", "full-time", "hard-working", "need-a-life") )
 
 str(data)
 
-zerules <- apriori(data, parameter = list(minlen=2, supp=0.1, conf = 0.7), appearance = list(rhs=c("income=small", "income=large"), default="lhs"),control = list(verbose=F))
+data <- as(data, "transactions")
+
+head(data)
+as(data, "data.frame")
+
+
+zerules <- apriori(data, parameter = list(minlen=2, supp=0.1, conf = 0.6), appearance = list(rhs=c("income=small", "income=large"), default="lhs"),control = list(verbose=F))
 inspect(zerules)
 
 redundant <- is.redundant(zerules)
-rules.pruned <- zerules[redundant == F]
-rulesorted <- sort(rules.pruned, by="lift", decreasing = T)
+zerules.pruned <- zerules[redundant == FALSE]
+rulesorted <- sort(zerules.pruned, by="lift", decreasing = TRUE)
 inspect(rulesorted)
 
 #Scatter Plot
-plot(rules.pruned)
+plot(zerules.pruned)
 
 #Balloon plot
-plot(rules.pruned, method="graph", control=list(type="items"))
+plot(zerules.pruned, method="graph", control=list(type="items"))
+
+#Parallel plot
+plot(zerules.pruned, method="paracoord", control=list(reorder=T))
+
+
