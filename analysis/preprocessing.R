648db11d67e91ab6399b586916efc3dbe8e24744
Landcruiser87 <andyheroy@gmail.com>
2019-08-10 15:09:01 -0500

    Adding more data cleaning

--- analysis/preprocessing.R
+++ analysis/preprocessing.R
@@ -11,21 +11,41 @@ print(NA_sum)
 
 #remove whitespace from education
 levels(df$education) <- trimws(levels(df$education))
+levels(df$workclass) <- trimws(levels(df$workclass))
+levels(df$marital_status) <- trimws(levels(df$marital_status))
+levels(df$occupation) <- trimws(levels(df$occupation))
+levels(df$relationship) <- trimws(levels(df$relationship))
+levels(df$race) <- trimws(levels(df$race))
+levels(df$gender) <- trimws(levels(df$gender))
+levels(df$native_country) <- trimws(levels(df$native_country))
+levels(df$income_bracket) <- trimws(levels(df$income_bracket))
 
 
+
+
+#Bin Education into 3 bins.
 df$education <- fct_collapse(df$education,
         "No Diploma" = c("1st-4th", "5th-6th","7th-8th","9th", "10th", "11th", "12th", "Preschool"),
         Associates = c("Assoc-acdm", "Assoc-voc"),
         Diploma = c("Some-college", "HS-grad")
     )
-
+#Remap income bracket levels
 df$income_bracket <- fct_collapse(df$income_bracket,
-                             small = " <=50K.",
-                             large = " >50K."
+                             small = "<=50K.",
+                             large = ">50K."
 )
 
+#remove ? and replace with other
+levels(df$workclass)[levels(df$workclass)=="?"] <- "Other"
+levels(df$workclass)[levels(df$workclass)=="(Other) "] <- "Other"
 
-levels(df$education)
-levels(df$income_bracket)
+levels(df$occupation)[levels(df$occupation)=="?"] <- "Other-service"
+levels(df$native_country)[levels(df$native_country)=="?"] <- "Other"
+levels(df$native_country)[levels(df$native_country)=="(Other)"] <- "Other"
 
+
+levels(df$native_country)
+  
+summary(df)
 # levels(df$education)[levels(df$education)==" HS-grad"] <- "Diploma"
+
