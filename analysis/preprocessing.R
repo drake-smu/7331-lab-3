e383ae9bc16b7e1f5725e5d56d5fc91da70b4ae5
Landcruiser87 <andyheroy@gmail.com>
2019-08-10 15:16:09 -0500

    WHY YOU NO WORK

--- analysis/preprocessing.R
+++ analysis/preprocessing.R
@@ -37,15 +37,13 @@ df$income_bracket <- fct_collapse(df$income_bracket,
 
 #remove ? and replace with other
 levels(df$workclass)[levels(df$workclass)=="?"] <- "Other"
-levels(df$workclass)[levels(df$workclass)=="(Other) "] <- "Other"
+levels(df$workclass)[levels(df$workclass)==" (Other)"] <- "Other"
 
 levels(df$occupation)[levels(df$occupation)=="?"] <- "Other-service"
 levels(df$native_country)[levels(df$native_country)=="?"] <- "Other"
 levels(df$native_country)[levels(df$native_country)=="(Other)"] <- "Other"
 
-
-levels(df$native_country)
-  
+levels(df$workclass)
 summary(df)
 # levels(df$education)[levels(df$education)==" HS-grad"] <- "Diploma"
 
