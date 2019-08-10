3997c762cfafd097bec0ebedcac94edef882f589
Landcruiser87 <andyheroy@gmail.com>
2019-08-10 02:06:39 -0500

    Its late Isn't it great never submit to hate for thou shalt always be late
    IN LIFE.
    
    This is brought to you by, stupidity.

--- analysis/preprocessing.R
+++ analysis/preprocessing.R
@@ -1,40 +1,31 @@
 library(dplyr)
-
+library(forcats)
 
 #------Data Specific Cleaning------------
 
 df <- read.csv("./data/adult-test.csv")
 
-summary(df)
-
 #Lets look at NA value's first.
 NA_sum <- sort(sapply(df, function(x) sum(is.na(x))), decreasing = TRUE)
 print(NA_sum)
 
-Nodip <- c(" 1st-4th" , " 5th-6th" ," 7th-8th" ," 9th" , " 10th" , " 11th" , " 12th" , " Preschool" )
+#remove whitespace from education
+levels(df$education) <- trimws(levels(df$education))
 
-levels(df$education)[levels(df$education)==" 1st-4th"] <- "No Diploma"
-levels(df$education)[levels(df$education)==" 5th-6th"] <- "No Diploma"
-levels(df$education)[levels(df$education)==" 7th-8th"] <- "No Diploma"
-levels(df$education)[levels(df$education)==" 9th"] <- "No Diploma"
-levels(df$education)[levels(df$education)==" 10th"] <- "No Diploma"
-levels(df$education)[levels(df$education)==" 11th"] <- "No Diploma"
-levels(df$education)[levels(df$education)==" 12th"] <- "No Diploma"
-levels(df$education)[levels(df$education)==" PreSchool"] <- "No Diploma"
+
+df$education <- fct_collapse(df$education,
+        "No Diploma" = c("1st-4th", "5th-6th","7th-8th","9th", "10th", "11th", "12th", "Preschool"),
+        Associates = c("Assoc-acdm", "Assoc-voc"),
+        Diploma = c("Some-college", "HS-grad")
+    )
+
+df$income_bracket <- fct_collapse(df$income_bracket,
+                             small = " <=50K.",
+                             large = " >50K."
+)
+
 
 levels(df$education)
+levels(df$income_bracket)
 
-
-
-
-# c("1st-4th","5th-6th")
-# recode_factor(df$education, "1st-4th"="No Diploma",
-#                        "5th-6th"="No Diploma",
-#                        "7th-8th"="No Diploma",
-#                        "9th" = "No Diploma",
-#                        "10th" = "No Diploma",
-#                        "11th" = "No Diploma",
-#                        "12th" = "No Diploma",
-#                        "Preschool" = "No Diploma",
-#                        "Assoc-admin" = "Associates")
-
+# levels(df$education)[levels(df$education)==" HS-grad"] <- "Diploma"
