989061c31aca0e0ec1a0ae359fe74ead65fff631
Landcruiser87 <andyheroy@gmail.com>
2019-08-10 19:45:21 -0500

    plots!

--- analysis/preprocessing.R
+++ analysis/preprocessing.R
@@ -1,6 +1,8 @@
 library(dplyr)
 library(forcats)
-
+library(pander)
+library(ggplot2)
+library(cowplot)
 #------Data Specific Cleaning------------
 
 ## @knitr dataimport
@@ -54,3 +56,45 @@ levels(data$native_country)[levels(data$native_country)=="?"] <- "Other"
 ## @knitr preprocessres
 levels(data$workclass)
 pander(summary(data))
+
+## @knitr EDAGraphs
+#Looking at Age
+p1 <- ggplot(data, aes(x = age, color = income_bracket, fill = income_bracket)) + 
+  geom_density(alpha = 0.9) +
+  labs(x = "Age", y = "Density", title = "The older you get, the more you make",
+       subtitle = "Density plot")
+
+#Looking at education
+p2 <- ggplot(data, aes(x = education, fill = income_bracket, color = income_bracket)) +
+  geom_bar(alpha = 0.9, position = "fill") +
+  coord_flip() +
+  labs(x = "Education", y = "Proportion", title = "Income bias based on Education",
+       subtitle = "Stacked bar plot")
+
+#Marital Status
+p3 <- ggplot(data, aes(x = marital_status, fill = income_bracket, color = income_bracket)) +
+  geom_bar(alpha = 0.9, position = "fill") +
+  coord_flip() +
+  labs(x = "Marital Status", y = "Proportion", title = "Income bias based on Marital status",
+       subtitle = "Stacked bar plot")
+
+#Occupation
+p4 <- ggplot(data, aes(x = occupation, fill = income_bracket, color = income_bracket)) +
+  geom_bar(alpha = 0.9, position = "fill") +
+  coord_flip() +
+  labs(x = "Occupation Status", y = "Proportion", title = "Income bias based on Occupation status",
+       subtitle = "Stacked bar plot")
+
+
+# 
+# #Occupation
+# p4 <- ggplot(data, aes(education)) + 
+#   geom_bar(aes(fill=income_bracket), width = 0.5) + 
+#   theme(axis.text.x = element_text(angle=65, vjust=0.6)) + 
+#   labs(title="Histogram on Occupation", 
+#        subtitle="Occupation vs income")
+
+p1
+p2
+p3
+p4
